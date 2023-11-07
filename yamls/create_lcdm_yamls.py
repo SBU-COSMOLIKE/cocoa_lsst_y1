5fcc268372dfedbfba8605de1d7b2e5bf73e292f
Joao <joao.reboucas1@gmail.com>
2023-11-07 15:34:29 -0300

    Adding LCDM yamls

---
+++ yamls/create_lcdm_yamls.py
@@ -0,0 +1,70 @@
+template_file = "MCMC34.yaml"
+
+with open(template_file, "r") as f:
+    template = f.read()
+
+# Change in template:
+# - output
+# - emulator
+# - num_refs
+# - fiducial and mask
+
+with open("yamls.txt", "r") as f:
+    lines = f.read().splitlines()
+lines = [line for line in lines if line != ""]
+
+for line in lines:
+    code, rest = line.split(" - ")
+    emu, mask, model, anchors, point = rest.split(" / ")
+
+    if emu == "EE2":
+        emu_code = 1
+    elif emu == "COLA":
+        emu_code = 5
+    else:
+        print(f"Unknown emulator {emu}")
+        exit(1)
+    
+    if anchors == "--":
+        num_anchors = 1
+    elif anchors == "1 anchor":
+        num_anchors = 1
+    elif anchors == "25 anchors":
+        num_anchors = 25
+    else:
+        print(f"Unsupported num of anchors emulator {anchors}")
+        exit(1)
+
+    if point == "EE2 ref":
+        dataset_file = "LSST_Y1_M1_EE2_NOISED.dataset"
+    elif point == "High Omega_m High As":
+        dataset_file = "LSST_Y1_M1_OM_HIGH_AS_HIGH.dataset"
+    elif point == "Low Omega_m High As":
+        dataset_file = "LSST_Y1_M1_OM_LOW_AS_HIGH.dataset"
+    elif point == "High Omega_m Low As":
+        dataset_file = "LSST_Y1_M1_OM_HIGH_AS_LOW.dataset"
+    elif point == "Low Omega_m Low As":
+        dataset_file = "LSST_Y1_M1_OM_LOW_AS_LOW.dataset"
+    elif point == "High Omega_m High ns":
+        dataset_file = "LSST_Y1_M1_OM_HIGH_NS_HIGH.dataset"
+    elif point == "Low Omega_m High ns":
+        dataset_file = "LSST_Y1_M1_OM_LOW_NS_HIGH.dataset"
+    elif point == "High Omega_m Low ns":
+        dataset_file = "LSST_Y1_M1_OM_HIGH_NS_LOW.dataset"
+    elif point == "Low Omega_m Low ns":
+        dataset_file = "LSST_Y1_M1_OM_LOW_NS_LOW.dataset"
+    else:
+        print(f"Unsupported fiducial point {point}")
+        exit(1)
+    
+    assert mask in ["M2", "M3", "M4"], f"Mask {mask} not a valid mask"
+
+    dataset_file = dataset_file.replace("M1", mask)
+
+    contents = template.replace("MCMC34", f"MCMC{code}")
+    contents = contents.replace("non_linear_emul: 5", f"non_linear_emul: {emu_code}")
+    contents = contents.replace("num_refs: 1", f"num_refs: {num_anchors}")
+    contents = contents.replace("LSST_Y1_M1_OM_HIGH_AS_HIGH.dataset", dataset_file)
+
+    with open(f"MCMC{code}.yaml", "w") as f:
+        f.write(contents)
