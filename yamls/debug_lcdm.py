4715a3564ae2b1f1a0c2b4c39da5afb9001f21c5
Joao <joao.reboucas1@gmail.com>
2023-11-13 16:33:55 -0300

    Debug lcdm chains

--- yamls/debug_lcdm.py
+++ yamls/debug_lcdm.py
@@ -1,12 +1,14 @@
-codes = range(207, 286)
+codes = range(206, 286)
 
 for code in codes:
 	with open(f"MCMC{code}.yaml", "r") as f:
 		lines = f.readlines()
 
 	for i, line in enumerate(lines):
-		if "As_1e9, ns, H0, omegab, omegam, w " in line:
-			lines[i] = "            As_1e9, ns, H0, omegab, omegam\n"
+		if "w: -1" in line: break
+	
+	lines = lines[:i+1] + ["  wa: 0\n"] + lines[i+1:]
 		
 	with open(f"MCMC{code}.yaml", "w") as f:
 		f.writelines(lines)
+	
