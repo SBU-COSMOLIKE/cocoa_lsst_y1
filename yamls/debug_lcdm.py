ccb62b613ad77f19cc463276b824105fe0becbab
Joao <joao.reboucas1@gmail.com>
2023-11-13 16:28:01 -0300

    Debugging lcdm yamls

--- yamls/debug_lcdm.py
+++ yamls/debug_lcdm.py
@@ -1,15 +1,12 @@
-codes = range(206, 286)
+codes = range(207, 286)
 
 for code in codes:
 	with open(f"MCMC{code}.yaml", "r") as f:
 		lines = f.readlines()
 
 	for i, line in enumerate(lines):
-		if "num_refs:" in line:
-			assert lines[i+1] == "\n"
-			lines[i+1] = "    cola_emu_mode: LCDM\n"
-			break
+		if "As_1e9, ns, H0, omegab, omegam, w " in line:
+			lines[i] = "            As_1e9, ns, H0, omegab, omegam\n"
 		
 	with open(f"MCMC{code}.yaml", "w") as f:
 		f.writelines(lines)
-	
