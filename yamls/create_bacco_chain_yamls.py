6f9625ccdba577eb81a687f36851663bf804cd24
Joao <joao.reboucas1@gmail.com>
2024-06-12 18:04:33 -0300

    Correcting

--- yamls/create_bacco_chain_yamls.py
+++ yamls/create_bacco_chain_yamls.py
@@ -1,5 +1,5 @@
 indices = range(526, 538)
-new_indices = range(724, 736)
+new_indices = range(712, 724)
 for i, j in zip(indices, new_indices):
     with open(f"MCMC{i}.yaml", "r") as f:
         contents = f.read()
@@ -12,7 +12,7 @@ for i, j in zip(indices, new_indices):
             break
     new_dataset_file = dataset_file.split(".")[0] + "_BACCO.dataset"
     contents = contents.replace(dataset_file, new_dataset_file)
-    contents = contents.replace("non_linear_emul: 7", "non_linear_emul: 6")
-    contents = contents.replace("num_refs: 0", "num_refs: 500")
+    contents = contents.replace("non_linear_emul: 7", "non_linear_emul: 8")
+    # contents = contents.replace("num_refs: 0", "num_refs: 500")
     with open(f"MCMC{j}.yaml", "w") as f:
         f.write(contents)
