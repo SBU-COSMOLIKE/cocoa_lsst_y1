a8e1a085b4fde0374a5a65c98d6e085b826ae1d3
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-05-17 14:31:04 -0400

    Update

--- likelihood/_cosmolike_prototype_base.py
+++ likelihood/_cosmolike_prototype_base.py
@@ -49,16 +49,34 @@ class _cosmolike_prototype_base(DataSetLikelihood):
     self.theta_max_arcmin = ini.float("theta_max_arcmin")
     
     # ------------------------------------------------------------------------
+ 
+    self.nz_interp_1d=int(500 + 250*self.accuracyboost)
+    # EUCLID EMULATOR CAN ONLY HANDLE 100 Z's BELOW Z=10
+    max_nz_interp_2d=150
+    if self.non_linear_emul == 1:
+      max_nz_interp_2d=99
+    else: 
+      max_nz_interp_2d=150
+    self.nz_interp_2d=int(min(60 + 15*self.accuracyboost,150))
     
-    self.z_interp_1D = np.linspace(0,2.0,1000)
-    self.z_interp_1D = np.concatenate((self.z_interp_1D,
-      np.linspace(2.0,10.1,200)),axis=0)
-    self.z_interp_1D = np.concatenate((self.z_interp_1D,
-      np.linspace(1080,2000,20)),axis=0) #CMB 6x2pt g_CMB (possible in the future)
-    self.z_interp_1D[0] = 0
+    self.nk_interp_2d=int(500 + 250*self.accuracyboost)
 
-    self.z_interp_2D = np.linspace(0,2.0,120)
-    self.z_interp_2D = np.concatenate((self.z_interp_2D, np.linspace(2.01,10,30)),axis=0)
+    self.z_interp_1D = np.linspace(0, 3.0, max(100,int(0.80*self.nz_interp_2d)))
+    self.z_interp_1D = np.concatenate(
+      (self.z_interp_1D,
+      np.linspace(3.0,10.1,max(100,int(0.20*self.nz_interp_2d)))),
+      axis=0)
+    self.z_interp_1D = np.concatenate(
+      (self.z_interp_1D,
+      np.linspace(1080, 2000, 20)),
+      axis=0) #CMB 6x2pt g_CMB (possible in the future)
+    self.z_interp_1D[0] = 0
+    
+    self.z_interp_2D = np.linspace(0,3.0,max(10,int(0.85*self.nz_interp_2d)))
+    self.z_interp_2D = np.concatenate(
+      (self.z_interp_2D, 
+       np.linspace(3.01, 10, max(10,int(0.15*self.nz_interp_2d)))), 
+       axis=0)
     self.z_interp_2D[0] = 0
 
     self.len_z_interp_2D = len(self.z_interp_2D)
