43c6e2a6009b8de916cfeb3b43b39c123211cb03
Joao Victor Rebouças <joaoreboucas@che.cbpf.br>
2025-05-29 12:53:50 -0300

    Adding EE2 at the projected cosmology as an emulator option

--- likelihood/_cosmolike_prototype_base.py
+++ likelihood/_cosmolike_prototype_base.py
@@ -324,6 +324,35 @@ class _cosmolike_prototype_base(DataSetLikelihood):
       logboost_2d = logboost_2d_interp(self.z_interp_2D, log10k_interp_2D)
       logboost_2d[self.z_interp_2D > 3, :] = 0.0
       lnPNL = lnPL + logboost_2d.T.flatten()
+    if self.non_linear_emul == 5:
+      # EE2 at the projected LCDM cosmology
+      params = {
+        'Omm'  : self.provider.get_param("omegam"),
+        'As'   : self.provider.get_param("As"),
+        'Omb'  : self.provider.get_param("omegab"),
+        'ns'   : self.provider.get_param("ns"),
+        'h'    : h,
+        'mnu'  : self.provider.get_param("mnu"), 
+        'w'    : -1,
+        'wa'   : 0
+      }
+
+      kbt = np.power(10.0, np.linspace(-2.0589, 0.973, self.len_k_interp_2D))
+      kbt, tmp_bt = self.emulator.get_boost(params, self.z_interp_2D, kbt)
+      logkbt = np.log10(kbt)
+
+      for i in range(self.len_z_interp_2D):    
+        interp = interp1d(logkbt,
+            np.log(tmp_bt[i]), 
+            kind = 'linear', 
+            fill_value = 'extrapolate', 
+            assume_sorted = True
+          )
+
+        lnbt = interp(log10k_interp_2D)
+        lnbt[np.power(10,log10k_interp_2D) < 8.73e-3] = 0.0
+    
+        lnPNL[i::self.len_z_interp_2D]  = lnPL[i::self.len_z_interp_2D] + lnbt
     else:
       raise LoggedError(self.log, "non_linear_emul = %d is an invalid option", self.non_linear_emul)
 
