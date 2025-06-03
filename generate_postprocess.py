e57c4e4aba1b60b2b37915828a0a493cc177ca77
João <joao.reboucas1@gmail.com>
2025-06-03 19:52:00 -0300

    Adding new postprocess chains

--- generate_postprocess.py
+++ generate_postprocess.py
@@ -6,6 +6,7 @@
 import os
 import yaml
 from copy import deepcopy
+import sys
 
 def diff_nested_dicts(dict1, dict2, label1, label2):
     """
@@ -24,6 +25,39 @@ def diff_nested_dicts(dict1, dict2, label1, label2):
     return diff
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("ERROR: you must provide chain indices to generate the postprocess")
+        print("Example: python3 generate_postprocess.py 1-3,5-7,8,9,10 COLA")
+        print("This would generate postprocess for chains 1, 2, 3, 5, 6, 7, 8, 9 and 10 using the COLA emulator")
+        print("If the chain is not EE2, the postprocess is not generated.")
+        exit(1)
+
+    if len(sys.argv) == 2:
+        print("ERROR: you must provide the emulator to postprocess the chains")
+        print("Example: python3 generate_postprocess.py 1-3,5-7,8,9,10 COLA")
+        print("This would generate postprocess for chains 1, 2, 3, 5, 6, 7, 8, 9 and 10 using the COLA emulator")
+        print("If the chain is not EE2, the postprocess is not generated.")
+        exit(1)
+
+    # Parsing the indices
+    inp = sys.argv[1].split(",")
+    indices = []
+    for entry in inp:
+        if "-" in entry:
+            low, high = list(map(int, entry.split("-")))
+            for i in range(low, high+1): indices.append(i)
+        else:
+            indices.append(int(entry))
+    
+    # Parse the emulator
+    emu = sys.argv[2]
+    available_emus = {"COLA": 3, "EE2proj": 5, "EE2": 1}
+    if emu not in available_emus:
+        print(f"ERROR: emulator must be one of {list(available_emus.keys())} but you provided {emu}")
+        exit(1)
+    
+    print(f"Generating postprocess yamls for chains {indices} using emulator {emu}")
+
     # Open documentation file
     with open("CHAINS.md", "r") as f:
         lines = f.readlines()
@@ -32,6 +66,7 @@ if __name__ == "__main__":
         if line.startswith("#"): continue
         fields = line[2:].split(" | ")
         index = int(fields[0])
+        if index not in indices: continue
         emulator = fields[1].strip()
         fid_cosmo = fields[2].strip()
         scale_cuts = fields[3].strip()
@@ -40,7 +75,9 @@ if __name__ == "__main__":
         except IndexError:
             extra_info = None
 
-        if emulator != "EE2": continue
+        if emulator != "EE2":
+            print(f"WARNING: requested postprocess of chain {index}, but this chain has the emulator {emulator}. Skipping...")
+            continue
 
         if not os.path.exists(f"./yamls/MCMC{index}.yaml"):
             print(f"ERROR: Yaml for MCMC {index} does not exist, cannot generate postprocess file.")
@@ -60,7 +97,7 @@ if __name__ == "__main__":
         postprocess_yaml["post"] = {
             "skip": 0.5,
             "thin": 15,
-            "suffix": "POSTCOLA",
+            "suffix": emu,
             "remove": {
                 "theory": postprocess_yaml["theory"],
                 "likelihood": deepcopy(mcmc_yaml["likelihood"]),
@@ -70,8 +107,8 @@ if __name__ == "__main__":
                 "likelihood": deepcopy(mcmc_yaml["likelihood"]),
             },
         }
-        postprocess_yaml["post"]["add"]["likelihood"]["lsst_y1.lsst_y1_cosmic_shear"]["non_linear_emul"] = 3
+        postprocess_yaml["post"]["add"]["likelihood"]["lsst_y1.lsst_y1_cosmic_shear"]["non_linear_emul"] = available_emus[emu]
 
-        with open(f"./yamls/POST{index}_COLA.yaml", "w") as f:
+        with open(f"./yamls/POST{index}_{emu}.yaml", "w") as f:
             yaml.dump(postprocess_yaml, f, default_flow_style=False)
         
