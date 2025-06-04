98c84185ab95f2fedccfdf36f59723fec0a1847c
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-06-04 12:49:51 -0400

    adjust start/stop.sh

--- scripts/start_lsst_y1.sh
+++ scripts/start_lsst_y1.sh
@@ -10,14 +10,20 @@ if [ -z "${IGNORE_COSMOLIKE_LSSTY1_CODE}" ]; then
   # Parenthesis = run in a subshell
   ( source "${ROOTDIR:?}/installation_scripts/flags_check.sh" ) || return 1;
 
-  export LD_LIBRARY_PATH="${ROOTDIR:?}/projects/lsst_y1/interface":${LD_LIBRARY_PATH}
+  PROJECT="${ROOTDIR:?}/projects"
+  FOLDER="${XXX_NAME:-"xxx"}"
+  PACKDIR="${PROJECT:?}/${FOLDER:?}"
 
-  export PYTHONPATH="${ROOTDIR:?}/projects/lsst_y1/interface":${PYTHONPATH}
+  export LD_LIBRARY_PATH="${FOLDER:?}/interface":${LD_LIBRARY_PATH}
+
+  export PYTHONPATH="${FOLDER:?}/interface":${PYTHONPATH}
 
   if [ -n "${COSMOLIKE_DEBUG_MODE}" ]; then
       export SPDLOG_LEVEL=debug
   else
       export SPDLOG_LEVEL=info
   fi
+
+  unset -v PROJECT FOLDER PACKDIR
 
 fi
